603221c2003fb22547eb6efaea5b7793073de60f
kavya subramani <kavya.291991@gmail.com>
2019-11-12 12:35:12 -0800

    BinaryTree problems

--- src/nodes/TreeNode.java
+++ src/nodes/TreeNode.java
@@ -5,4 +5,12 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val;
+    }
 }
